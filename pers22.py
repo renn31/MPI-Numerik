31f83d8b0def4a91329410b0c9058a91d8707b73
Iren Veronika Sirait <128461789+renn31@users.noreply.github.com>
2023-11-12 22:05:57 +0700

    Add files via upload

--- pers22.py
+++ pers22.py
@@ -1,27 +1,53 @@
-import time
-def bubble_sort(arr):
-    arr_len = len(arr)
+from mpi4py import MPI
+import math
 
-    for i in range(arr_len - 1):
-        flag = 0
+# Inisialisasi MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
-        for j in range(0, arr_len - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                flag = 1
+# Input koefisien a, b, dan c dari proses 0
+a = None
+b = None
+c = None
 
-        if flag == 0:
-            break
+if rank == 0:
+    a = float(input("Masukkan koefisien a: "))
+    b = float(input("Masukkan koefisien b: "))
+    c = float(input("Masukkan koefisien c: "))
 
-    return arr
-time_start=time.time()
+# Broadcast koefisien a, b, dan c dari proses 0 ke semua proses
+a = comm.bcast(a, root=0)
+b = comm.bcast(b, root=0)
+c = comm.bcast(c, root=0)
 
-arr = [10, 6, 8, 3, 2, 7, 5, 4, 1, 9]
-sorted_arr = bubble_sort(arr)
-print("List sorted with bubble sort in ascending order:", sorted_arr)
+# Hitung diskriminan di semua proses
+diskriminan = b**2 - 4*a*c
+# Inisialisasi variabel untuk menerima hasil
+x1 = None
+x2 = None
 
-time_end=time.time()
+# Cek apakah diskriminan positif, nol, atau negatif di semua proses
+if diskriminan > 0:
+    x1_local = (-b + math.sqrt(diskriminan)) / (2*a)
+    x2_local = (-b - math.sqrt(diskriminan)) / (2*a)
+elif diskriminan == 0:
+    x1_local = -b / (2*a)
+    x2_local = x1_local
+else:
+    realPart = -b / (2*a)
+    imaginaryPart = math.sqrt(-diskriminan) / (2*a)
+    x1_local = complex(realPart, imaginaryPart)
+    x2_local = complex(realPart, -imaginaryPart)
 
-waktu_eksekusi=time_end-time_start
+# Kumpulkan hasil dari semua proses ke proses 0
+x1 = comm.gather(x1_local, root=0)
+x2 = comm.gather(x2_local, root=0)
 
-print ("waktu eksekusi=",waktu_eksekusi)
+# Di proses 0, cetak hasil akar-akar persamaan kuadrat
+if rank == 0:
+    print("Akar-akar persamaan kuadrat adalah:")
+    for i in range(size):
+        print(f"Proses {i}:")
+        print("x1 =", x1[i])
+        print("x2 =", x2[i])
